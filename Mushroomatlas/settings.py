1de130d2b10ee3adf5cc82659d7a7bf188efa9ba
Wolferine69 <vasek.censored@centrum.cz>
2024-06-19 20:32:43 +0200

    pip freeze

--- Mushroomatlas/settings.py
+++ Mushroomatlas/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'Mushroomatlas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mushroomatlas',
+        'USER': 'postgres.gqvpdffwubmpwphrixvk',
+        'PASSWORD': '21mushroomatlas+',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
