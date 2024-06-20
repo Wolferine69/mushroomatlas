a2c3bc8115a24425248863a6a92a481d929c86e5
Wolferine69 <vasek.censored@centrum.cz>
2024-06-20 18:13:11 +0200

    admin panel

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from viewer.models import *
+
 # Register your models here.
+admin.site.register(Family)
+admin.site.register(Mushroom)
+admin.site.register(Finding)
+admin.site.register(Recipe)
+admin.site.register(Comment)
