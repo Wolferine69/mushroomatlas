dcfb499e58413ec485f9ac104bae1aaee66d97db
Wolferine69 <vasek.censored@centrum.cz>
2024-06-20 20:30:35 +0200

    funkcni login uzivatele

--- Mushroomatlas/urls.py
+++ Mushroomatlas/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from accounts.views import SubmittableLoginView
+from viewer.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 class SubmittableLoginView(LoginView):
     template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
