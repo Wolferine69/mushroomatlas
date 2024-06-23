807875d850ce4dc90656f6a3b59ad7575cc67ff0
Wolferine69 <vasek.censored@centrum.cz>
2024-06-23 08:17:22 +0200

    fix: views

--- viewer/views.py
+++ viewer/views.py
@@ -14,44 +14,44 @@ class MushroomListView(ListView):
     template_name = 'mushroom_list.html'
     context_object_name = 'mushrooms'
 
+
 class MushroomDetailView(DetailView):
     model = Mushroom
     template_name = 'mushroom_detail.html'
     context_object_name = 'mushroom'
+
 
 class FamilyListView(ListView):
     model = Family
     template_name = 'family_list.html'
     context_object_name = 'families'
 
+
 class FamilyDetailView(DetailView):
     model = Family
     template_name = 'family_detail.html'
     context_object_name = 'family'
+
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipes_list.html'
     context_object_name = 'recipes'
 
+
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
     context_object_name = 'recipe'
 
-class MushroomDetailView(DetailView):
-    model = Mushroom
-    template_name = 'mushroom_detail.html'
-    context_object_name = 'mushroom'
 
 class TipListView(ListView):
     model = Tip
     template_name = 'tip_list.html'
     context_object_name = 'tips'
 
+
 class TipDetailView(DetailView):
     model = Tip
     template_name = 'tip_detail.html'
     context_object_name = 'tip'
-
-
