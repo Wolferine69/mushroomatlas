aa55c7904a8546e6c88f111061a363d27e3b9c93
Zdeněk Havránek <havranek.zd@gmail.com>
2024-07-10 01:16:26 +0200

    feature: phase 4

--- messaging/forms.py
+++ messaging/forms.py
@@ -4,7 +4,7 @@ from .models import Message
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
-        fields = ['receiver', 'content', 'replied_to']
+        fields = ['receiver', 'subject', 'content', 'replied_to']
         widgets = {
             'replied_to': forms.HiddenInput()
         }
--- messaging/models.py
+++ messaging/models.py
@@ -4,9 +4,11 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages_messaging', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages_messaging', on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255, default='No Subject')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     replied_to = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Message from {self.sender} to {self.receiver} at {self.timestamp}"
+
