f5ca8965e5fd1a001bfbebeeb451b7364b6800bd
Zdeněk Havránek <havranek.zd@gmail.com>
2024-07-11 14:58:46 +0200

    change: add cz label

--- messaging/forms.py
+++ messaging/forms.py
@@ -5,6 +5,12 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['receiver', 'subject', 'content', 'replied_to']
+        labels = {
+            'receiver': 'Příjemce',
+            'subject': 'Předmět',
+            'content': 'Text',
+            'replied_to': 'Odeslat',
+        }
         widgets = {
             'replied_to': forms.HiddenInput()
         }
@@ -13,5 +19,8 @@ class AttachmentForm(forms.ModelForm):
     class Meta:
         model = Attachment
         fields = ['file']
+        labels = {
+            'file': 'Soubor',
+        }
 
 AttachmentFormSet = forms.inlineformset_factory(Message, Attachment, form=AttachmentForm, extra=1, can_delete=True)
