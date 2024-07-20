e50df3b9aaacee560cf1e97b95512dab9b267ce3
Zdeněk Havránek <havranek.zd@gmail.com>
2024-07-21 00:36:07 +0200

    test: added another messaging test

--- messaging/tests.py
+++ messaging/tests.py
@@ -8,33 +8,100 @@ from viewer.models import Profile
 
 class UserProfileTest(TestCase):
     def setUp(self):
-        # Vytvoření testovacích uživatelů
+        self.client = Client()
         self.user1 = User.objects.create_user(username='user1', password='pass')
         self.user2 = User.objects.create_user(username='user2', password='pass')
-
-        # Vytvoření profilů pro testovací uživatele
         self.profile1 = Profile.objects.create(user=self.user1)
         self.profile2 = Profile.objects.create(user=self.user2)
+        self.message = Message.objects.create(
+            sender=self.user2,  # Odesílatel je user2
+            receiver=self.user1,  # Příjemce je user1
+            subject='Test Subject',
+            content='Test Message',
+            is_trashed_by_receiver=False,  # Ujistěme se, že zpráva není označena jako smazaná
+            is_deleted_by_receiver=False  # Ujistěme se, že zpráva není označena jako odstraněná
+        )
 
-        # Vytvoření testovací zprávy
-        self.message = Message.objects.create(sender=self.user1, receiver=self.user2, content='Test Message')
-
-    def test_send_message(self):
+    def test_reply_message(self):
         self.client.login(username='user1', password='pass')
-        response = self.client.post(reverse('send_message'), {
-            'receiver': self.user2.id,
-            'subject': 'Test Subject',
-            'content': 'Another Test Message',
+        response = self.client.post(reverse('reply_message', args=[self.message.pk]), {
+            'subject': 'Re: Test Subject',
+            'content': 'This is a reply message.',
+            'receiver': self.user2.pk,
+            'replied_to': self.message.pk,
             'attachments-TOTAL_FORMS': '1',
             'attachments-INITIAL_FORMS': '0',
             'attachments-MIN_NUM_FORMS': '0',
             'attachments-MAX_NUM_FORMS': '1000',
             'attachments-0-file': SimpleUploadedFile("file.txt", b"file_content")
         })
-        if response.status_code != 302:
-            print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(Message.objects.filter(content='Another Test Message').exists())
+        self.assertEqual(response.status_code, 302)  # Redirect to outbox
+        self.assertEqual(Message.objects.last().subject, 'Re: Test Subject')
+
+    def test_mark_message_read(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.get(reverse('mark_message_read', args=[self.message.pk]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.message.refresh_from_db()
+        self.assertTrue(self.message.is_read)
+
+    def test_view_inbox(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.get(reverse('view_inbox'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Subject')
+
+class MessagingViewsTest(UserProfileTest):
+    def setUp(self):
+        super().setUp()
+        self.client = Client()
+        self.client.login(username='user1', password='pass')
+        self.message = Message.objects.create(
+            sender=self.user1,
+            receiver=self.user2,
+            subject='Test Subject',
+            content='This is a test message.'
+        )
+
+    def test_view_outbox(self):
+        response = self.client.get(reverse('view_outbox'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Subject')
+
+    def test_view_trash(self):
+        self.message.is_trashed_by_sender = True
+        self.message.save()
+        response = self.client.get(reverse('view_trash'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Subject')
+
+    def test_send_message(self):
+        response = self.client.post(reverse('send_message'), {
+            'receiver': self.user2.pk,
+            'subject': 'New Test Subject',
+            'content': 'This is a new test message.',
+            'attachments-TOTAL_FORMS': '1',
+            'attachments-INITIAL_FORMS': '0',
+            'attachments-MIN_NUM_FORMS': '0',
+            'attachments-MAX_NUM_FORMS': '1000',
+            'attachments-0-file': SimpleUploadedFile("file.txt", b"file_content")
+        })
+        self.assertEqual(response.status_code, 302)  # Redirect to outbox
+        self.assertEqual(Message.objects.last().subject, 'New Test Subject')
+
+    def test_forward_message(self):
+        response = self.client.post(reverse('forward_message', args=[self.message.pk]), {
+            'receiver': self.user2.pk,
+            'subject': 'Fwd: Test Subject',
+            'content': 'This is a forwarded message.',
+            'attachments-TOTAL_FORMS': '1',
+            'attachments-INITIAL_FORMS': '0',
+            'attachments-MIN_NUM_FORMS': '0',
+            'attachments-MAX_NUM_FORMS': '1000',
+            'attachments-0-file': SimpleUploadedFile("file.txt", b"file_content")
+        })
+        self.assertEqual(response.status_code, 302)  # Redirect to outbox
+        self.assertEqual(Message.objects.last().subject, 'Fwd: Test Subject')
 
 class MessageModelTest(UserProfileTest):
     def test_create_message(self):
@@ -136,102 +203,3 @@ class ReceiverFilterFormTest(UserProfileTest):
     def test_receiver_filter_form(self):
         form = ReceiverFilterForm(user=self.user1)
         self.assertNotIn(self.user1, form.fields['receiver'].queryset)
-
-class MessagingViewsTest(UserProfileTest):
-    def setUp(self):
-        super().setUp()
-        self.client = Client()
-        self.client.login(username='user1', password='pass')
-        self.message = Message.objects.create(
-            sender=self.user1,
-            receiver=self.user2,
-            subject='Test Subject',
-            content='This is a test message.'
-        )
-
-    # def test_view_inbox(self):
-    #     response = self.client.get(reverse('view_inbox'))
-    #     if 'Test Subject' not in response.content.decode():
-    #         print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, 'Test Subject')
-
-    # TODO AssertionError: False is not true : Couldn't find 'Test Subject' in response
-
-    def test_view_outbox(self):
-        response = self.client.get(reverse('view_outbox'))
-        if 'Test Subject' not in response.content.decode():
-            print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Test Subject')
-
-    def test_view_trash(self):
-        self.message.is_trashed_by_sender = True
-        self.message.save()
-        response = self.client.get(reverse('view_trash'))
-        if 'Test Subject' not in response.content.decode():
-            print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Test Subject')
-
-    def test_send_message(self):
-        response = self.client.post(reverse('send_message'), {
-            'receiver': self.user2.pk,
-            'subject': 'New Test Subject',
-            'content': 'This is a new test message.',
-            'attachments-TOTAL_FORMS': '1',
-            'attachments-INITIAL_FORMS': '0',
-            'attachments-MIN_NUM_FORMS': '0',
-            'attachments-MAX_NUM_FORMS': '1000',
-            'attachments-0-file': SimpleUploadedFile("file.txt", b"file_content")
-        })
-        if response.status_code != 302:
-            print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-        self.assertEqual(response.status_code, 302)  # Redirect to outbox
-        self.assertEqual(Message.objects.last().subject, 'New Test Subject')
-
-    def test_forward_message(self):
-        response = self.client.post(reverse('forward_message', args=[self.message.pk]), {
-            'receiver': self.user2.pk,
-            'subject': 'Fwd: Test Subject',
-            'content': 'This is a forwarded message.',
-            'attachments-TOTAL_FORMS': '1',
-            'attachments-INITIAL_FORMS': '0',
-            'attachments-MIN_NUM_FORMS': '0',
-            'attachments-MAX_NUM_FORMS': '1000',
-            'attachments-0-file': SimpleUploadedFile("file.txt", b"file_content")
-        })
-        if response.status_code != 302:
-            print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-        self.assertEqual(response.status_code, 302)  # Redirect to outbox
-        self.assertEqual(Message.objects.last().subject, 'Fwd: Test Subject')
-
-    # def test_reply_message(self):
-    #     response = self.client.post(reverse('reply_message', args=[self.message.pk]), {
-    #         'subject': 'Re: Test Subject',
-    #         'content': 'This is a reply message.',
-    #         'receiver': self.user1.pk,
-    #         'replied_to': self.message.pk,
-    #         'attachments-TOTAL_FORMS': '1',
-    #         'attachments-INITIAL_FORMS': '0',
-    #         'attachments-MIN_NUM_FORMS': '0',
-    #         'attachments-MAX_NUM_FORMS': '1000',
-    #         'attachments-0-file': SimpleUploadedFile("file.txt", b"file_content")
-    #
-    #     })
-    #     if response.status_code != 302:
-    #         print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-    #     self.assertEqual(response.status_code, 302)  # Redirect to outbox
-    #     self.assertEqual(Message.objects.last().subject, 'Re: Test Subject')
-
-    # TODO AssertionError: 200 != 302
-
-    # def test_mark_message_read(self):
-    #     response = self.client.get(reverse('mark_message_read', args=[self.message.pk]), follow=True)
-    #     if response.status_code != 200:
-    #         print(response.content.decode())  # Debugovací výstup pro obsah odpovědi
-    #     self.assertEqual(response.status_code, 200)
-    #     self.message.refresh_from_db()
-    #     self.assertTrue(self.message.is_read)
-
-    # TODO TypeError: mark_message_read()go tan unexpected keyword argument 'message_id'
