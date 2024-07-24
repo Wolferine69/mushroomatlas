9b8a012a4f1e89faaa903becf56bbd91419392b8
Zdeněk Havránek <havranek.zd@gmail.com>
2024-07-24 10:53:21 +0200

    fix: messages and tests, + add tests

--- messaging/views.py
+++ messaging/views.py
@@ -1,7 +1,7 @@
 import os
 
 from django.contrib import messages
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
